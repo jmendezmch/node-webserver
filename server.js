9c3102088a3e392e00f32a7bb0ba8f841fdcf58d
Javier Mendez <jmendezmch@gmail.com>
2018-09-08 20:31:36 -0300

    primer commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+
+app.get('/', function(req, res) {
+
+    res.render('home', {
+        nombre: 'javier mendez',
+        anio: new Date().getFullYear()
+    });
+});
+
+app.get('/about', function(req, res) {
+
+    res.render('about', {
+        anio: new Date().getFullYear()
+    });
+});
+
+
+app.listen(port, () => {
+    console.log('Escuchando en el puerto ' + port);
+});
